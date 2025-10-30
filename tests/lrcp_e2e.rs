a872656bfe59e491e306e08609f729b52371cbe0
zwpdbh <hyperion_z@outlook.com>
2025-10-30 14:28:32 +0800

    > fix retransmit

--- tests/lrcp_e2e.rs
+++ tests/lrcp_e2e.rs
@@ -22,9 +22,13 @@ mod line_reversal_tests {
         });
     }
 
-    async fn send_and_recv(socket: &UdpSocket, server_addr: &str, msg: &str) -> Result<String> {
+    async fn udp_send(socket: &UdpSocket, server_addr: &str, msg: &str) -> Result<()> {
         socket.send_to(msg.as_bytes(), server_addr).await?;
 
+        Ok(())
+    }
+
+    async fn udp_recv(socket: &UdpSocket) -> Result<String> {
         let mut buf = [0; 1024];
         let (len, _) = timeout(Duration::from_secs(2), socket.recv_from(&mut buf))
             .await
@@ -51,56 +55,80 @@ mod line_reversal_tests {
 
         // 1. Connect
         let connect_msg = format!("/connect/{SESSION_ID}/");
-        let ack = send_and_recv(&client_socket, &server_addr, &connect_msg).await?;
-        assert_eq!(ack, format!("/ack/{SESSION_ID}/0/"));
+        let _ = udp_send(&client_socket, &server_addr, &connect_msg).await?;
+
+        // let ack = send_and_recv(&client_socket, &server_addr, &connect_msg).await?;
+        assert_eq!(
+            udp_recv(&client_socket).await?,
+            format!("/ack/{SESSION_ID}/0/")
+        );
 
         // 2. Send "hello\n"
-        let data1 = format!("/data/{SESSION_ID}/0/hello\n/");
-        let ack1 = send_and_recv(&client_socket, &server_addr, &data1).await?;
-        assert_eq!(ack1, format!("/ack/{SESSION_ID}/6/",));
+        let _ = udp_send(
+            &client_socket,
+            &server_addr,
+            &format!("/data/{SESSION_ID}/0/hello\n/"),
+        )
+        .await?;
+        // let ack1 = send_and_recv(&client_socket, &server_addr, &data1).await?;
+        assert_eq!(
+            udp_recv(&client_socket).await?,
+            format!("/ack/{SESSION_ID}/6/",)
+        );
 
         // 3. Expect reversed "olleh\n" from server
-        let mut buf = [0; 1024];
-        let (len, _) = timeout(Duration::from_secs(2), client_socket.recv_from(&mut buf))
-            .await
-            .map_err(|_| Error::Other("Timeout waiting for server data".into()))??;
-        let server_data1 = String::from_utf8_lossy(&buf[..len]).to_string();
-        // Server sends: /data/12345/0/olleh\n/
-        // Note: \n is literal newline, so escaped as \\n in string
-        assert_eq!(server_data1, format!("/data/{SESSION_ID}/0/olleh\n/"));
+        assert_eq!(
+            udp_recv(&client_socket).await?,
+            format!("/data/{SESSION_ID}/0/olleh\n/")
+        );
 
         // Ack the server's data
-        let ack_server_data = format!("/ack/{SESSION_ID}/6/");
-        client_socket
-            .send_to(ack_server_data.as_bytes(), &server_addr)
-            .await?;
+        let _ = udp_send(
+            &client_socket,
+            &server_addr,
+            &format!("/ack/{SESSION_ID}/6/"),
+        )
+        .await?;
 
         // 4. Send "Hello, world!\n"
-        let data2 = format!("/data/{SESSION_ID}/6/Hello, world!\n/");
-        let ack2 = send_and_recv(&client_socket, &server_addr, &data2).await?;
-        assert_eq!(ack2, format!("/ack/{SESSION_ID}/20/"));
+        let _ = udp_send(
+            &client_socket,
+            &server_addr,
+            &format!("/data/{SESSION_ID}/6/Hello, world!\n/"),
+        )
+        .await?;
+
+        assert_eq!(
+            udp_recv(&client_socket).await?,
+            format!("/ack/{SESSION_ID}/20/")
+        );
 
         // 5. Expect reversed "!dlrow ,olleH\n"
-        let (len, _) = timeout(Duration::from_secs(2), client_socket.recv_from(&mut buf))
-            .await
-            .map_err(|_| Error::Other("Timeout waiting for second server data".into()))??;
-
-        let server_data2 = String::from_utf8_lossy(&buf[..len]).to_string();
         assert_eq!(
-            server_data2,
+            udp_recv(&client_socket).await?,
             format!("/data/{SESSION_ID}/6/!dlrow ,olleH\n/")
         );
 
         // Ack it
-        let ack_server_data2 = format!("/ack/{SESSION_ID}/20/",);
-        client_socket
-            .send_to(ack_server_data2.as_bytes(), &server_addr)
-            .await?;
+        let _ = udp_send(
+            &client_socket,
+            &server_addr,
+            &format!("/ack/{SESSION_ID}/20/"),
+        )
+        .await?;
 
         // 6. Close session
-        let close_msg = format!("/close/{SESSION_ID}/");
-        let close_resp = send_and_recv(&client_socket, &server_addr, &close_msg).await?;
-        assert_eq!(close_resp, format!("/close/{SESSION_ID}/"));
+        let _ = udp_send(
+            &client_socket,
+            &server_addr,
+            &format!("/close/{SESSION_ID}/"),
+        )
+        .await?;
+
+        assert_eq!(
+            udp_recv(&client_socket).await?,
+            format!("/close/{SESSION_ID}/")
+        );
 
         // Cancel server (it's designed to run forever, so we just drop the task)
         server_handle.abort();
@@ -125,37 +153,135 @@ mod line_reversal_tests {
         let server_addr = format!("127.0.0.1:{}", SERVER_PORT);
 
         // 1. Connect
-        let connect_msg = format!("/connect/{SESSION_ID}/");
-        let ack = send_and_recv(&client_socket, &server_addr, &connect_msg).await?;
-        assert_eq!(ack, format!("/ack/{SESSION_ID}/0/"));
+        let _ = udp_send(
+            &client_socket,
+            &server_addr,
+            &format!("/connect/{SESSION_ID}/"),
+        )
+        .await?;
+
+        assert_eq!(
+            udp_recv(&client_socket).await?,
+            format!("/ack/{SESSION_ID}/0/")
+        );
 
         // 2. Send "hello "
-        let data1 = format!("/data/{SESSION_ID}/0/hello /");
-        let ack1 = send_and_recv(&client_socket, &server_addr, &data1).await?;
-        assert_eq!(ack1, format!("/ack/{SESSION_ID}/6/",));
+        let _ = udp_send(
+            &client_socket,
+            &server_addr,
+            &format!("/data/{SESSION_ID}/0/hello /"),
+        )
+        .await?;
+
+        assert_eq!(
+            udp_recv(&client_socket).await?,
+            format!("/ack/{SESSION_ID}/6/",)
+        );
 
         // 3. Send "world!"
-        let data1 = format!("/data/{SESSION_ID}/6/world!/");
-        let ack1 = send_and_recv(&client_socket, &server_addr, &data1).await?;
-        assert_eq!(ack1, format!("/ack/{SESSION_ID}/12/",));
 
-        let data1 = format!("/data/{SESSION_ID}/12/\\/\n/");
-        let ack1 = send_and_recv(&client_socket, &server_addr, &data1).await?;
-        assert_eq!(ack1, format!("/ack/{SESSION_ID}/14/",));
-
-        let mut buf = [0; 1024];
-        let expected: String = "hello world!".chars().rev().collect();
-        let (len, _) = timeout(Duration::from_secs(2), client_socket.recv_from(&mut buf))
-            .await
-            .map_err(|_| Error::Other("Timeout waiting for second server data".into()))??;
-
-        let server_data2 = String::from_utf8_lossy(&buf[..len]).to_string();
+        let _ = udp_send(
+            &client_socket,
+            &server_addr,
+            &format!("/data/{SESSION_ID}/6/world!/"),
+        )
+        .await?;
         assert_eq!(
-            server_data2,
-            format!("/data/{SESSION_ID}/0/\\/{expected}\n/")
+            udp_recv(&client_socket).await?,
+            format!("/ack/{SESSION_ID}/12/",)
+        );
+
+        let _ = udp_send(
+            &client_socket,
+            &server_addr,
+            &format!("/data/{SESSION_ID}/12/\n/"),
+        )
+        .await?;
+        assert_eq!(
+            udp_recv(&client_socket).await?,
+            format!("/ack/{SESSION_ID}/13/",)
+        );
+
+        assert_eq!(
+            udp_recv(&client_socket).await?,
+            format!(
+                "/data/12345/0/{}/",
+                "hello world!".chars().rev().collect::<String>() + "\n"
+            ),
         );
 
         // Cancel server (it's designed to run forever, so we just drop the task)
+        server_handle.abort();
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    /// Test server retrasmits data if it doesn't receive acks
+    async fn test_retransmit() -> Result<()> {
+        let _x = init_tracing();
+
+        // Start the server in the background
+        let server_handle = tokio::spawn(async {
+            if let Err(e) = problem7::run(SERVER_PORT).await {
+                eprintln!("Server error: {:?}", e);
+            }
+        });
+
+        let client_socket = UdpSocket::bind("127.0.0.1:0").await?;
+        let server_addr = format!("127.0.0.1:{}", SERVER_PORT);
+
+        // 1. Connect
+        let _ = udp_send(
+            &client_socket,
+            &server_addr,
+            &format!("/connect/{SESSION_ID}/"),
+        )
+        .await?;
+        assert_eq!(
+            udp_recv(&client_socket).await?,
+            format!("/ack/{SESSION_ID}/0/")
+        );
+
+        // 2. Send "hello "
+        let _ = udp_send(
+            &client_socket,
+            &server_addr,
+            &format!("/data/{SESSION_ID}/0/hello\n/"),
+        )
+        .await?;
+
+        assert_eq!(
+            udp_recv(&client_socket).await?,
+            format!("/ack/{SESSION_ID}/6/",)
+        );
+
+        assert_eq!(
+            udp_recv(&client_socket).await?,
+            format!(
+                "/data/{SESSION_ID}/0/{}/",
+                "hello".chars().rev().collect::<String>() + "\n"
+            )
+        );
+
+        // --------------
+        // should resend if doesn't receive acks
+        // --------------
+        assert_eq!(
+            udp_recv(&client_socket).await?,
+            format!(
+                "/data/{SESSION_ID}/0/{}/",
+                "hello".chars().rev().collect::<String>() + "\n"
+            )
+        );
+        assert_eq!(
+            udp_recv(&client_socket).await?,
+            format!(
+                "/data/{SESSION_ID}/0/{}/",
+                "hello".chars().rev().collect::<String>() + "\n"
+            )
+        );
+
         server_handle.abort();
 
         Ok(())
